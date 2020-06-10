871d9fbc4764c1d7d25bee200f02e6edd1846f89
fumi uk <dcfumiya.238@gmail.com>
2020-06-10 09:08:19 +0900

    make front

--- src/main/java/com/example/chat/web/form/RoomForm.java
+++ src/main/java/com/example/chat/web/form/RoomForm.java
@@ -1,8 +1,8 @@
 package com.example.chat.web.form;
 
-import com.sun.istack.NotNull;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
